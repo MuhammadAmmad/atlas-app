076e694dc8f0572a287133d45e9f3f2cd7e3092a
xingfuryda <xingfuryda@users.noreply.github.com>
2016-12-02 17:31:29 -0700

    change to mapbox-gl based map

---
+++ webui/js/index.js
@@ -0,0 +1,48 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        console.log('Received Event: ' + 'deviceready');   
+        this.setupDocument();  
+        this.setupMap();   
+    },
+
+    setupDocument: function() {
+        // menu button
+        var menuButton = document.getElementsByClassName('menuButton')[0];        
+        menuButton.addEventListener('click', function(){
+            var menu = document.getElementsByClassName('menu')[0];            
+            var mapCover = document.getElementsByClassName('mapCover')[0];            
+            menu.classList.add('show');
+            mapCover.classList.add('show');            
+        });
+
+        // mapCover
+        var mapCover = document.getElementsByClassName('mapCover')[0];       
+        mapCover.addEventListener('click', function(){
+            var that = this; 
+            var menu = document.getElementsByClassName('menu')[0];
+            that.classList.remove('show');
+            menu.classList.remove('show');            
+        });
+    },
+
+    setupMap: function(){
+        mapboxgl.accessToken = 'pk.eyJ1IjoieGluZ2Z1cnlkYSIsImEiOiJhMWNmOWU2NGU5ODQ5NjE3Nzc4Y2ZiYjQzNzI0MTc5ZiJ9.iCaLgnYU9Se1gYtEPZBzEg';
+        var map = new mapboxgl.Map({
+            container: 'mapPane',
+            style: 'mapbox://styles/mapbox/streets-v9',
+            attributionControl: false
+        });
+    }    
+};
+
+app.initialize();
+
